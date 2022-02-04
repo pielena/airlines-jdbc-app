adc21a684639bfe2ff23f0909b6108fa56f949e9
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2022-02-04 17:16:12 +0300

    HLNA-1 Fix review comments

--- src/main/java/com/airlines/dao/DbConnector.java
+++ src/main/java/com/airlines/dao/DbConnector.java
@@ -1,22 +1,28 @@
-package com.airlines.database;
+package com.airlines.dao;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class JDBCConnection {
+public class DbConnector {
 
-    private Connection getDBConnection() {
+    public Connection getDBConnection() {
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
         String url = resourceBundle.getString("url");
         String username = resourceBundle.getString("username");
         String password = resourceBundle.getString("password");
 
+        HikariDataSource datasource = new HikariDataSource();
+        datasource.setJdbcUrl(url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+
         Connection dbConnection = null;
         try {
-            dbConnection = DriverManager.getConnection(url, username, password);
+            dbConnection = datasource.getConnection();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,34 +1,32 @@
 CREATE TABLE IF NOT EXISTS crew (
-id BIGINT NOT NULL AUTO_INCREMENT ,
-name VARCHAR(255) NOT NULL ,
+id BIGINT AUTO_INCREMENT ,
+name VARCHAR(50) NOT NULL ,
 
 CONSTRAINT crew_PK PRIMARY KEY (id)
 );
 
-
 CREATE TABLE IF NOT EXISTS airplane (
-id            BIGINT NOT NULL AUTO_INCREMENT ,
-code_name     VARCHAR(255) NOT NULL ,
-model         VARCHAR(255) NOT NULL ,
+id            BIGINT AUTO_INCREMENT ,
+code_name     VARCHAR(50) NOT NULL ,
+model         VARCHAR(50) NOT NULL ,
 manufacture_date      DATE NOT NULL ,
-capacity    BIGINT NOT NULL ,
+capacity    INT NOT NULL ,
 flight_range DECIMAL NOT NULL ,
-crew BIGINT NOT NULL ,
+crew_id BIGINT NOT NULL ,
 
 CONSTRAINT airplane_PK PRIMARY KEY (id),
-CONSTRAINT airplane_crew_FK FOREIGN KEY (crew) REFERENCES crew(id)
+CONSTRAINT airplane_crew_FK FOREIGN KEY (crew_id) REFERENCES crew(id)
 );
 
 CREATE TABLE IF NOT EXISTS crew_member (
-id BIGINT NOT NULL ,
-crew BIGINT NOT NULL ,
-first_name VARCHAR(255) NOT NULL ,
-last_name VARCHAR(255) NOT NULL ,
-position VARCHAR(255) NOT NULL ,
+id BIGINT AUTO_INCREMENT ,
+crew_id BIGINT NOT NULL ,
+first_name VARCHAR(50) NOT NULL ,
+last_name VARCHAR(50) NOT NULL ,
+position VARCHAR(50) NOT NULL ,
 birthday DATE NOT NULL ,
-citizenship VARCHAR(255) ,
+citizenship VARCHAR(25) ,
 
 CONSTRAINT crew_member_PK PRIMARY KEY (id) ,
-CONSTRAINT crew_member_crew_FK FOREIGN KEY (crew) REFERENCES crew(id)
+CONSTRAINT crew_member_crew_FK FOREIGN KEY (crew_id) REFERENCES crew(id)
 );
-
