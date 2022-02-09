8f61d1a09ce2d8aa9876fbe7bcce6e05b7a32a08
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2022-02-09 19:18:28 +0300

    HLNA-3 fix review comments

--- src/main/java/com/airlines/dao/impl/CrewMemberDaoImpl.java
+++ src/main/java/com/airlines/dao/impl/CrewMemberDaoImpl.java
@@ -1,5 +1,7 @@
-package com.airlines.dao;
+package com.airlines.dao.impl;
 
+import com.airlines.dao.CrewMemberDao;
+import com.airlines.dao.DbConnector;
 import com.airlines.exception.DaoOperationException;
 import com.airlines.model.Citizenship;
 import com.airlines.model.CrewMember;
@@ -15,15 +17,18 @@ import java.util.Objects;
 public class CrewMemberDaoImpl implements CrewMemberDao {
 
     private final static String INSERT_CREW_MEMBER_SQL =
-            "INSERT INTO crew_member(first_name, last_name, position, birthday, citizenship, crew_id, id) " +
-                    "VALUES(?,?,?,?,?,?,?);";
+            "INSERT INTO crew_member(id, first_name, last_name, position, birthday, citizenship, crew_id) " +
+                    "VALUES(?, ?, ?, ?, ?, ?, ?);";
     private final static String UPDATE_CREW_MEMBER_SQL =
             "UPDATE crew_member SET first_name = ?, last_name = ?, position = ?, birthday = ?, citizenship = ?, crew_id =? " +
                     "WHERE id = ?;";
     private final static String SELECT_CREW_MEMBER_BY_ID_SQL = "SELECT * FROM crew_member WHERE id = ?;";
 
-    private final DbConnector dbConnector = new DbConnector();
+    private final DbConnector dbConnector;
 
+    public CrewMemberDaoImpl(DbConnector dbConnector) {
+        this.dbConnector = dbConnector;
+    }
 
     @Override
     public void save(CrewMember crewMember) {
@@ -32,24 +37,18 @@ public class CrewMemberDaoImpl implements CrewMemberDao {
         try (Connection connection = dbConnector.getDBConnection()) {
 
             PreparedStatement insertStatement = connection.prepareStatement(INSERT_CREW_MEMBER_SQL);
-            fillStatementWithCrewMemberData(insertStatement, crewMember);
-            insertStatement.setInt(7, crewMember.getId());
+            insertStatement.setInt(1, crewMember.getId());
+            insertStatement.setString(2, crewMember.getFirstName());
+            insertStatement.setString(3, crewMember.getLastName());
+            insertStatement.setString(4, crewMember.getPosition().toString());
+            insertStatement.setDate(5, Date.valueOf(crewMember.getBirthday()));
+            insertStatement.setString(6, crewMember.getCitizenship().toString());
+            insertStatement.setInt(7, crewMember.getCrewId());
             insertStatement.executeUpdate();
 
         } catch (SQLException e) {
-            throw new DaoOperationException("Error saving crew member" + crewMember);
+            throw new DaoOperationException("Error saving crew member" + crewMember, e);
         }
-    }
-
-    private void fillStatementWithCrewMemberData(PreparedStatement insertStatement, CrewMember crewMember)
-            throws SQLException {
-
-        insertStatement.setString(1, crewMember.getFirstName());
-        insertStatement.setString(2, crewMember.getLastName());
-        insertStatement.setString(3, crewMember.getPosition().toString());
-        insertStatement.setDate(4, Date.valueOf(crewMember.getBirthday()));
-        insertStatement.setString(5, crewMember.getCitizenship().toString());
-        insertStatement.setInt(6, crewMember.getCrewId());
     }
 
     @Override
@@ -63,17 +62,20 @@ public class CrewMemberDaoImpl implements CrewMemberDao {
         try (Connection connection = dbConnector.getDBConnection()) {
 
             PreparedStatement updateStatement = connection.prepareStatement(UPDATE_CREW_MEMBER_SQL);
-            fillStatementWithCrewMemberData(updateStatement, crewMember);
+            updateStatement.setString(1, crewMember.getFirstName());
+            updateStatement.setString(2, crewMember.getLastName());
+            updateStatement.setString(3, crewMember.getPosition().toString());
+            updateStatement.setDate(4, Date.valueOf(crewMember.getBirthday()));
+            updateStatement.setString(5, crewMember.getCitizenship().toString());
+            updateStatement.setInt(6, crewMember.getCrewId());
             updateStatement.setInt(7, crewMember.getId());
             int rowsAffected = updateStatement.executeUpdate();
 
             if (rowsAffected == 0) {
                 throw new DaoOperationException(String.format("Crew member with id = %d does not exist", crewMember.getId()));
             }
-
         } catch (SQLException e) {
-            throw new DaoOperationException(String.format("Crew member with id = %d does not exist", crewMember.getId()));
-
+            throw new DaoOperationException(String.format("Crew member with id = %d does not exist", crewMember.getId()), e);
         }
     }
 
@@ -85,22 +87,21 @@ public class CrewMemberDaoImpl implements CrewMemberDao {
             selectByIdStatement.setInt(1, id);
             ResultSet resultSet = selectByIdStatement.executeQuery();
 
-            if (resultSet.next()) {
-                CrewMember crewMember = CrewMember.builder()
-                        .withId(resultSet.getInt("id"))
-                        .withCrewId(resultSet.getInt("crew_id"))
-                        .withFirstName(resultSet.getString("first_name"))
-                        .withLastName(resultSet.getString("last_name"))
-                        .withPosition(Position.valueOf(resultSet.getString("position")))
-                        .withBirthday(resultSet.getDate("birthday").toLocalDate())
-                        .withCitizenship(Citizenship.valueOf(resultSet.getString("citizenship")))
-                        .build();
-                return crewMember;
-            } else {
+            if (!resultSet.next()) {
                 throw new DaoOperationException(String.format("Crew member with id = %d does not exist", id));
             }
+
+            return CrewMember.builder()
+                    .withId(resultSet.getInt("id"))
+                    .withCrewId(resultSet.getInt("crew_id"))
+                    .withFirstName(resultSet.getString("first_name"))
+                    .withLastName(resultSet.getString("last_name"))
+                    .withPosition(Position.valueOf(resultSet.getString("position")))
+                    .withBirthday(resultSet.getDate("birthday").toLocalDate())
+                    .withCitizenship(Citizenship.valueOf(resultSet.getString("citizenship")))
+                    .build();
         } catch (SQLException e) {
-            throw new DaoOperationException(String.format("Error finding crew member by id = %d", id));
+            throw new DaoOperationException(String.format("Error finding crew member by id = %d", id), e);
         }
     }
 }
